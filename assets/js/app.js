a2947bfdb8edff392d5c08c9fccdc12f299e5d87
Daniil <negativdaniil4@gmail.com>
2022-12-23 18:13:45 +0200

    Projec init

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+let a;
+let b;
+let result;
+let operator;
+let pi;
+
+
+a = prompt('R', '');
+b = prompt('h', '');
+operator = prompt('operator', '');
+pi = 3, 14
+
+
+
+switch (operator) {
+   case '*':
+      result = (Number(pi) * (Number(a) * Number(a))) * Number(b);
+      break;
+}
+
+
+
+alert(result);
